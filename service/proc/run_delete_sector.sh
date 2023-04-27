925aa825644848e5629848ef7bd79de59a0b621b
ruz76 <jan.ruzicka@cybele.space>
2023-04-27 09:26:15 +0200

    Polishing.

--- service/proc/run_delete_sector.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-export PATH
-unset PYTHONPATH
-unset GISBASE
-unset LD_LIBRARY_PATH
-#printenv
-python $2/grass/delete_sector.py $1 $2 $3 $4
