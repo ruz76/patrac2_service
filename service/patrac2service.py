32965de85d5e95bbeeae27f8db55dc9c2666bb8d
ruz76 <jan.ruzicka@cybele.space>
2024-12-06 14:50:45 +0100

    Fixed version

--- service/patrac2service.py
+++ service/patrac2service.py
@@ -342,7 +342,7 @@ def delete_sector():
 
 @app.route("/version", methods=['GET'])
 def version():
-    resp = Response(response=json.dumps({"version": "2024-12-05"}),
+    resp = Response(response=json.dumps({"version": "2024-12-06"}),
                     status=200,
                     mimetype="application/json")
     return resp
